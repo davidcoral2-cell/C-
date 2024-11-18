032db5b3c94a256df28a02af48021455cc6196a0
davidcoral2-cell <gtbyy2@gmail.com>
2024-11-18 18:13:49 +0100

    Ej4

---
+++ EjClase/Ej4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string a;
+    string b;
+
+    cout << "Como te llamas? ";
+    cin >> a;
+    cout << "Y tu apellido?";
+    cin >> b;
+    cout << "Hola " << a << ", " << b;
+
+}
